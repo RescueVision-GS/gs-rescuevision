8a7c9e33aaaa67dadd3750de81fad3bc43c6f7ef
Daniel <kanashiitami66@gmail.com>
2025-06-04 04:02:12 -0300

    JS/CHATBOT corrigindo bug

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-<script>
-  window.watsonAssistantChatOptions = {
+window.watsonAssistantChatOptions = {
     integrationID: "7ce0c7d3-09fc-4e7d-9d65-ed1a25044f3e", // The ID of this integration.
     region: "us-south", // The region your integration is hosted in.
     serviceInstanceID: "57d89cae-105e-4184-ae9b-f484ec15e365", // The ID of your service instance.
@@ -10,4 +9,4 @@
     t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
     document.head.appendChild(t);
   });
-</script>
+  
